51861847568a4c6967df87c2d737809f8c9a4ed5
taylor <taylor@localhost.localdomain>
2025-07-21 11:27:28 -0400

    Add some basic JS and CSS for adding task
    Current model is not efficient think about maybe seperating and turning
    into a better function call. May need to rethink logic? Possibly by
    spliting them or redoing the current HTML Layout.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 const task = document.querySelector("#task");
+const addTask = document.querySelector(".addTask");
 
 task.checked && task.classList.add("checked");
-
 task.addEventListener("click", () => task.classList.toggle("checked"));
+
+addTask.addEventListener("click", () => {
+  const newTask = document.createElement("input");
+  newTask.classList.add("newInput");
+  addTask.parentElement.appendChild(newTask);
+  newTask.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+
+    }
+  })
+})
